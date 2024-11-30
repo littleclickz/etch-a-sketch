536175cebc049136fad178c738674bf6b91dee6c
littleclickz <hnvanpatten@gmail.com>
2024-11-29 20:12:22 -0800

    Set initial grid size and flex attributes

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const divContainer = document.querySelector("#container"); // defines our grid c
 
 function initialGrid() {
     let i = 0;
-    while (i < 17) {
+    while (i < 16) {
         const newDiv = document.createElement("div");
         divContainer.appendChild(newDiv);
         newDiv.id = "box";
+        newDiv.style.minWidth = "246px";
+        newDiv.style.maxWidth = "246px";
+        newDiv.style.minHeight = "246px";
+        newDiv.style.maxHeight = "246px";
         i ++;
     }
 }
