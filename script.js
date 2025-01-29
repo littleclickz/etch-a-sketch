e8c4270997fef8dd7c050762b0dff99296f806b8
littleclickz <hnvanpatten@gmail.com>
2025-01-28 19:39:11 -0800

    Add comments to prep for next steps

--- script.js
+++ script.js
@@ -22,21 +22,27 @@ function initialGrid() {
 
 initialGrid();
 
+//everything above this line works! next: focus on figuring out how to remove original grid
+//(something to do with .getElementsByClassName returning an array, so you have to loop through the array
+//and delete each array item)
+
 function userGrid() {
     const originalGrid = document.getElementsByClassName("box");
     originalGrid.remove();
-    const userInput = document.getElementById("choice").value;
-    let i = 0;
-    while (i < userInput && i <= 100) {
-        const newDiv = document.createElement("div");
-        divContainer.appendChild(newDiv);
-        newDiv.className = "box";
-        newDiv.addEventListener("mouseover", () => {
-            newDiv.style.backgroundColor = "lavender";
-        });
-        i ++;
-    }
+    //const userInput = document.getElementById("choice").value;
+    //let i = 0;
+    //while (i < userInput && i <= 100) {
+        //const newDiv = document.createElement("div");
+        //divContainer.appendChild(newDiv);
+        //newDiv.className = "box";
+        //newDiv.addEventListener("mouseover", () => {
+            //newDiv.style.backgroundColor = "lavender";
+        //});
+        //i ++;
+    //}
 }
+
+//everything below this line works!
 
 const formSubmit = document.querySelector("#subBtn");
 formSubmit.addEventListener("click", () => {
